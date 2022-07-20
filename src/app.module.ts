ba9df19bda781509b94806ccf08dd9b9fdbf09e1
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2022-07-20 18:12:14 +0200

    created auth and student module, created student entity accordingly to requirements - please check

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HrModule } from './hr/hr.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { StudentService } from './student/student.service';
+import { StudentController } from './student/student.controller';
 
 @Module({
    imports: [
@@ -19,7 +23,7 @@ import { HrModule } from './hr/hr.module';
       }),
       HrModule,
    ],
-   controllers: [AppController],
-   providers: [AppService],
+   controllers: [AppController, AuthController, StudentController],
+   providers: [AppService, AuthService, StudentService],
 })
 export class AppModule {}
